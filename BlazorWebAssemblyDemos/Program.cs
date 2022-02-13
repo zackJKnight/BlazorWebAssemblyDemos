4a386fa6a61c28c4c819191c8da33120873aab21
zachary knight <zachary.j.knight@gmail.com>
2022-02-13 14:58:16 -0600

    one dandelion

--- BlazorWebAssemblyDemos/Program.cs
+++ BlazorWebAssemblyDemos/Program.cs
@@ -1,11 +1,22 @@
 using BlazorWebAssemblyDemos;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Material;
+using Blazorise.Icons.Material;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+      .AddBlazorise(options =>
+      {
+          options.ChangeTextOnKeyPress = true;
+      })
+      .AddMaterialProviders()
+      .AddMaterialIcons();
 
 await builder.Build().RunAsync();
